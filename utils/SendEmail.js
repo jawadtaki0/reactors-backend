5f9f55f963f8d5a29fb1858721aea9815e884674
jawadtaki0 <jawadtaki0@gmail.com>
2025-12-06 16:07:01 +0200

    updated email OTP

---
+++ utils/SendEmail.js
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+
+export const sendEmail = async (to, subject, html) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER, // reactors.project@gmail.com
+        pass: process.env.EMAIL_PASS, // Gmail App Password
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent to:", to);
+    return true;
+  } catch (error) {
+    console.error("Email sending failed:", error.message);
+    return false;
+  }
+};
