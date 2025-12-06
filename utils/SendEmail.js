cfe0d3bdd23a6bff976c401398ba058539d8ba57
jawadtaki0 <jawadtaki0@gmail.com>
2025-12-06 16:43:09 +0200

    attempted fix 2: email not being sent //changed transport service to gmail

--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -7,9 +7,7 @@ export async function sendEmail(to, subject, html) {
 
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
